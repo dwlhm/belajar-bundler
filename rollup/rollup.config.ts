a3d5280fc9f9cb87570da51682479a7a82bed349
dwlhm <dwiilhamm026@gmail.com>
2022-08-23 09:20:55 +0700

    menambahkan konfigurasi minimal untuk solid-js

--- rollup/rollup.config.ts
+++ rollup/rollup.config.ts
@@ -3,21 +3,39 @@ import babel from '@rollup/plugin-babel';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 
-export default {
-    input: 'dist/tsc/src/index.js',
-    output: {
-        file: 'dist/final/main.min.js',
-        format: 'cjs',
-        sourcemap: 'inline',
-        exports: 'default'
+export default [
+    {
+        input: 'dist/tsc/src/index.js',
+        output: {
+            file: 'dist/final/main.min.js',
+            format: 'cjs',
+            sourcemap: 'inline',
+            exports: 'default'
+        },
+        plugins: [
+            resolve(),
+            commonjs(),
+            babel({
+            exclude: 'node_modules/**',
+            babelHelpers: 'bundled'
+            }),
+        ],
+        external: ['express']
     },
-    plugins: [
-        resolve(),
-        commonjs(),
-        babel({
-        exclude: 'node_modules/**',
-        babelHelpers: 'bundled'
-        }),
-    ],
-    external: ['express']
-};
+    {
+        input: 'dist/tsc/src/views/index.jsx',
+        output: {
+            file: 'dist/dist/js/views.min.js',
+            format: 'esm',
+            sourcemap: 'inline',
+            exports: 'auto'
+        },
+        plugins: [
+            resolve(),
+            commonjs(),
+            babel({
+                babelHelpers: 'bundled',
+            }),
+        ]
+    }
+];
